98ddb908ee3bbff734319c8e8623df8e39bd9585
Douglas Balkin <dbalkin77@gmail.com>
2017-10-07 21:24:01 -0500

    orm all method created

---
+++ burger/config/orm.js
@@ -0,0 +1,10 @@
+const connection = require('./connection.js');
+
+let orm = {
+    all: function(tableInput, cb){
+        connection.query('SELECT * FROM ' + tableInput+';', function(err, result){
+            if(err)throw err;
+            cb(result)
+        })
+    }
+}
