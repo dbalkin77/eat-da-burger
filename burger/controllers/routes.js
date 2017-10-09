6b03a0b19b048e3b240e4febf9b4dfbeb6084317
Douglas Balkin <dbalkin77@gmail.com>
2017-10-08 19:55:16 -0500

    app now functional

--- burger/controllers/routes.js
+++ burger/controllers/routes.js
@@ -7,14 +7,21 @@ router.get('/', function(req, res){
         console.log(burger_data);
         res.render('index', {burger_data});
     })
+})
 
 router.put('/burgers/update', function(req,res){
     burger.update(req.body.burger_id, function(result){
         console.log(result);
         res.redirect('/');
+    });
+}); 
+
+router.post('/burgers/create', function(req,res){
+    burger.create(req.body.burger_name, function(result){
+        res.redirect('/');
     })
-}) 
-    
 })
+
+
 
 module.exports = router; 
